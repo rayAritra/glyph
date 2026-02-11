c73e208c10cdd21c9a77e659efef63c79d0b907d
rayAritra <145261693+rayAritra@users.noreply.github.com>
2026-02-11 21:25:06 +0530

    03:database setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,14 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Geist, Inter, Darker_Grotesque } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
-import { dark } from '@clerk/themes'
-import { ThemeProvider } from "@/components/theme-provider";
+import {Providers} from "@/components/providers";
+
+
 import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
 
 const inter = Inter({
   variable: "--font-inter",
@@ -37,42 +31,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-        theme: dark,
-
-
-
-      }}
-    >
+   
       <html lang="en" suppressHydrationWarning>
         <body
           className={`${inter.variable} ${plexMono.variable} antialiased`}
         >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange>
+        <Providers>
+          
+          {children}
+         
 
-            <header>
-              <SignedOut>
-                <SignInButton />
-                <SignUpButton>
-                  <button className='bg-rose-500 text-white p-2 rounded'>
-                    Sign Up
-                  </button>
-                </SignUpButton>
-              </SignedOut>
 
-              <SignedIn>
-                <UserButton />
-              </SignedIn>
-            </header>
-            {children}
-          </ThemeProvider>
+        </Providers>
+          
         </body>
       </html>
-    </ClerkProvider>
+    
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
-import { Button } from "@/components/ui/button"
+"use client"
+import { useMutation, useQuery } from "convex/react";
+import { Button } from "@/components/ui/button";
+import { api } from "../../convex/_generated/api";
 const x = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
   return (
-    <div >
-      <Button variant="default">
-        click me
+    <div className="flex flex-col gap-2">
+      <Button onClick={() => createProject({ name: "New Project123" })}  >
+        Add new
       </Button>
+
+      {projects?.map((project) => (
+        <div key={project._id} className="border rounded p-2 p-4 flex flex-col">
+          <h1>{project.name}</h1>
+          <p>Owner ID: {project.ownerId}</p>
+        </div>
+      ))}
+
     </div>
   )
 }
