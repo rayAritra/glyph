92640c34f5cdd3f5b6a2a76fe5aa25cd724c4d7e
rayAritra <145261693+rayAritra@users.noreply.github.com>
2026-02-15 23:53:32 +0530

    06: error tracking

--- src/app/demo/page.tsx
+++ src/app/demo/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { useState } from "react"
-
+import * as Sentry from "@sentry/nextjs";
+import { useAuth } from "@clerk/nextjs";
 
 export default function Demopage() {
+    const userId = useAuth();
     const [loading, setLoading] = useState(false);
     const [loading1, setloading1] = useState(false);
     const handleBlocking = async () => {
@@ -17,6 +19,21 @@ export default function Demopage() {
         await fetch("/api/demo/background", { method: "POST" })
         setloading1(false);
     }
+
+    const handleClientError = () => {
+        Sentry.logger.info("User attempting to click on client function", {userId})
+        throw new Error("Client Error: something went wrong in the browser");
+        
+
+    };
+    const handleApiError = async () => {
+        await fetch("/api/demo/error", { method: "POST" })
+    };
+    const handleInngestError = async () => {
+        await fetch("/api/demo/inngest-error", { method: "POST" })
+    };
+
+
     return (
         <div className="p-8 space-x-4">
             <Button disabled={loading} onClick={handleBlocking}>
@@ -24,6 +41,24 @@ export default function Demopage() {
             </Button>
             <Button disabled={loading1} onClick={handleBackground}>
                 {loading1 ? "loading.." : "Background"}
+            </Button>
+            <Button
+            variant="destructive"
+            onClick={handleClientError}
+            >
+                Client Error
+            </Button>
+            <Button
+            variant="destructive"
+            onClick={handleApiError}
+            >
+                API Error
+            </Button>
+            <Button
+            variant="destructive"
+            onClick={handleInngestError}
+            >
+                Inngest Error
             </Button>
         </div>
     )
--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -1,4 +1,6 @@
 import { Inngest } from "inngest";
+import { sentryMiddleware } from "@inngest/middleware-sentry";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({ id: "glyph" });
+export const inngest = new Inngest({ id: "glyph", middleware: [sentryMiddleware()] });
+ 
